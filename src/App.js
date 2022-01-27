a8b44e7c2a3f234899926111d843bcf1639a1731
Matteo Nunez <matteo.nunez22@gmail.com>
2022-01-27 02:38:56 -0500

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio website in progress ...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/matteo-nunez-a27918139/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn profile
         </a>
       </header>
     </div>
