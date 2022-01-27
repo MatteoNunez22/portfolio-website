3ddd236bd1231198da4e5dba81b050d3075060c7
Matteo Nunez <matteo.nunez22@gmail.com>
2022-01-26 23:56:35 -0500

    Create react env

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfolio website in progress...
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/matteo-nunez-a27918139/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
