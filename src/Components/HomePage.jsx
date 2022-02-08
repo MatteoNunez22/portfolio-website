5578f83c56b1b6aa53e08571c086300dcf2eeedb
Matteo Nunez <matteo.nunez22@gmail.com>
2022-02-07 22:29:44 -0500

    Add portrait css class

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from '../media/portrait.jpg';
+import pfp from '../media/portrait.jpg';
 import '../App.css';
 
 class HomePage extends Component {
@@ -7,21 +7,21 @@ class HomePage extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Matteo Nunez
-                </p>
-                <p>
-                    Software Engineering student
-                </p>
-                <a
-                    className="App-link"
-                    href="https://www.linkedin.com/in/matteo-nunez-a27918139/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    LinkedIn profile
-                </a>
+                    <img src={pfp} className="Portrait" alt="Portrait pic" />
+                    <p>
+                        Matteo Nunez
+                    </p>
+                    <p>
+                        Software Engineering student
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://www.linkedin.com/in/matteo-nunez-a27918139/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn profile
+                    </a>
                 </header>
             </div>
         )
